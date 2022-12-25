5db5628224d30e6c5eb0c08ace6099800526d5fd
AFinlayV <afinlayv@gmail.com>
2022-12-25 17:44:31 -0500

    added some functions to gptools

--- gptools/__init__.py
+++ gptools/__init__.py
@@ -2,7 +2,6 @@ import openai
 import dalle2
 import requests
 import time
-import json
 
 
 def api_login():
@@ -19,26 +18,18 @@ def generate_text(prompt, model="text-davinci-003", temperature=0.7, n=1):
     return responses[0]
 
 
-
-
-
 def generate_text_from_image(image, model="text-davinci-003", temperature=0.5, n=1):
     completions = dalle2.generate_text(image, model=model, temperature=temperature, n=n)
     responses = [choice.text for choice in completions.choices]
     return responses[0]
 
 
-
-def refine_text(text, refine_by):
-    if refine_by == "":
-        refine_by = "make general improvements"
+def refine_text(text, refine_by="make general improvements"):
     prompt = f"[{text}] \n make a list of 5 ways to improve the text in brackets above, in the following way: {refine_by}\n"
     critique_text = generate_text(prompt)
-    # print(critique_text)
     prompt = f"[{text}]\n rewrite the text in brackets above, by addressing all of the following issues: \n{critique_text}\n\n"
     refined_text = generate_text(prompt)
-    return refined_text
-
+    return critique_text, refined_text
 
 
 def generate_story(plot, themes, characters, setting):
@@ -46,6 +37,11 @@ def generate_story(plot, themes, characters, setting):
         f"write a story about the following:\n plot:{plot} \n themes: {themes} \n characters: {characters} \n setting: {setting}")
     return story
 
+
+def prompt_assembler_text(preprompt, prompt, style):
+    full_prompt = f"{preprompt} /n {prompt} /n {style}"
+    response = generate_text(full_prompt)
+    return response
 
 def is_offensive(text):
     prompt = f"Is the following text offensive? \n {text} \n"
@@ -112,9 +108,3 @@ def generate_image_from_text(prompt, style, filename):
         print(f"{e}\nImage generation failed:\n {prompt} \n {style}\n trying again in 10 seconds...")
         time.sleep(10)
         generate_image_from_text(prompt, style, filename)
-
-
-
-
-
-
