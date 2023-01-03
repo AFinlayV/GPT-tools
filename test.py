19777bc3dc6a9c2c05a47aedbdff510eb478c1e1
AFinlayV <afinlayv@gmail.com>
2023-01-03 15:11:00 -0500

    tweaked the test script

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def main():
     """
     test the methods in ai.GPTtext object
     """
-    topic_list = ai.generate_list("good subjects for song lyrics", 1)
+    topic_list = ai.generate_list("unusual subjects for song lyrics", 2)
     for topic in topic_list:
         print(topic)
         gpt_prompt = ai.GPTprompt(f"write song about {topic}")
@@ -22,7 +22,7 @@ def main():
         gpt_text.title_type = "song"
         gpt_text.get_meta()
         for key in gpt_text.meta:
-            print(key, gpt_text.meta[key])
+            print(f"{key}:\n {gpt_text.meta[key]})
         print(f"\n~~~~~~NEW {gpt_text.title_type}~~~~~~~~\n")
         gpt_text.save(f"text/{gpt_text.title}.txt")
 
